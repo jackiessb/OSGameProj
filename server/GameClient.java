9554b953b77ee9631cbfdd41b15dd5ce56737ad1
jacksontorregrossa <jackiebehappy402@gmail.com>
2022-04-26 22:42:42 -0500

    fleshed out client

--- server/GameClient.java
+++ server/GameClient.java
@@ -2,18 +2,41 @@ package server;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 // Client-side gritty details for the Player 
 public class GameClient {
 	private Socket socket;
+	private BufferedReader reader;
+	private PrintWriter output;
+	private Scanner scanner = new Scanner(System.in);
 	
-	public GameClient(String address, int port) {
-		// tying socket together...
+	public GameClient(String address, int port) {		
+		// tying sockets together...
 		try {
-			this.socket = new Socket(address, port);
+			// establishing connection and input/output
+			socket = new Socket(address, port);
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			output = new PrintWriter(socket.getOutputStream());
+			
+			// if we connect...
+			String connected = reader.readLine();
+			System.out.println(connected);
+			
+			// giving user option to close the server
+			String input = scanner.nextLine();
+			output.println(input);
+			output.flush();
+			
+			socket.close();
+			
 		} catch (IOException e) {
 			System.out.println("Client could not connect!");
 			e.printStackTrace();
 		}
+	}
+	
+	public static void main(String[] args) {
+		GameClient player = new GameClient("192.168.0.16", 8300);
 	}
 }
